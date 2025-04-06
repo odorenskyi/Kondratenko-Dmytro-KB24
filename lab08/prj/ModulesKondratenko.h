793c6a911ea19122b15206e5b1396e78c95e9f8b
avemariax <yeq29880@gmail.com>
2025-04-06 12:36:41 +0300

    Analyzed tasks 9.1 - 9.3, added folder Report, docx. file

--- lab08/prj/ModulesKondratenko.h
+++ lab08/prj/ModulesKondratenko.h
@@ -4,5 +4,8 @@
 #include <cmath>
 
 double s_calculation(double x, double y, double z);
+double salary_calculation(double salary, double experience, double office_salary, double perce)
+void temperature_calculation(double avg_celsius, int size, double avg_fahrenheit, const double temps[])
+double analyze_bit(unsigned int N)
 
 #endif // MODULESKONDRATENKO_H_INCLUDED
--- lab08/prj/ModulesKondratenko/main.cpp
+++ lab08/prj/ModulesKondratenko/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <bitset>
 
 using namespace std;
 
@@ -10,3 +11,54 @@ double s_calculation(double x, double y, double z){
 
 }
 
+double salary_calculation(double salary, double experience, double office_salary, double result_salary){
+    cout << "¬вед≥ть розм≥р зароб≥тноњ плати(грн): " << endl;
+    cin >> salary;
+    cout << "¬вед≥ть розм≥р посадового окладу(грн): " << endl;
+    cin >> office_salary;
+    cout << "¬вед≥ть стаж роботи на п≥дприЇмств≥(роки): " << endl;
+    cin >> experience;
+
+    if(experience >= 3 && experience <= 5) {
+            office_salary * 10/100;
+    }
+    else if(experience >= 5 && experience <=7){
+            office_salary * 15/100;
+    }
+    else if(experience >= 7 && experience <= 15){
+            office_salary * 20/100;
+    }
+    else if(experience > 15) {
+            office_salary * 25/100;
+    }
+
+    result_salary = salary + office_salary;
+
+    cout << "—ума до виплати = " << result_salary << endl;
+
+}
+
+void temperature_calculation(double avg_celsius, int size, double avg_fahrenheit, const double temps[]){
+
+    double sum = 0.0;
+    cout << "¬вед≥ть температуру(у ∞C) в 00:00, 04:00, 08:00, 12:00, 16:00, 20:00 почергово: " << endl;
+    for (int i = 0; i < size; ++i) {
+        sum += temps[i];
+    }
+
+    avg_celsius = sum / size;
+    avg_fahrenheit = 32 + (9.0/5.0) *avg_celsius;
+}
+
+double analyze_bit(unsigned int N){
+
+        int bit11 = (N >> 11) & 1;
+        int count = 0;
+        bitset<32> bits(N);
+        for(int i = 0; i < 32; ++i){
+            if(bit11 == 0 && bits[i] == 0) count++;
+            if(bit11 == 1 && bits[i] == 1) count++;
+        }
+        return count;
+
+}
