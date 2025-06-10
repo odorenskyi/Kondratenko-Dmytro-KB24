35928ab239090c118de6bcb35caeb8555850dbd6
avemariax <yeq29880@gmail.com>
2025-06-10 22:07:08 +0300

    Added ClassLab12_Kondratenko into lab08/ModulesKondratenko

--- lab08/prj/ModulesKondratenko/ModulesKondratenko.h
+++ lab08/prj/ModulesKondratenko/ModulesKondratenko.h
@@ -17,4 +17,45 @@ string getFileSizeKB(const string& filename);
 string getCurrentDateTime();
 string toBinary(int b);
 
+class ClassLab12_Kondratenko {
+
+public:
+    ClassLab12_Kondratenko() : height(1), width(1), length(1) {}
+
+    ClassLab12_Kondratenko(double h, double l, double w) {
+        setHeight(h);
+        setLength(l);
+        setWidth(w);
+    }
+
+    double getHeight() const { return height; }
+    double getLength() const { return length; }
+    double getWidth() const { return width; }
+
+    void setHeight(double h) {
+        if (h < 0) throw std::invalid_argument("Висота прямокутника не може бути менше нуля!");
+        height = h;
+    }
+
+    void setLength(double l) {
+        if (l < 0) throw std::invalid_argument("Довжина прямокутника не може бути менше нуля!");
+        length = l;
+    }
+
+    void setWidth(double w) {
+        if (w < 0) throw std::invalid_argument("Ширина прямокутника не може бути менше нуля!");
+        width = w;
+    }
+
+    double getVolume() const {
+        return height * width * length;
+    }
+
+private:
+    double height;
+    double width;
+    double length;
+
+};
+
 #endif // MODULESKONDRATENKO_H_INCLUDED
